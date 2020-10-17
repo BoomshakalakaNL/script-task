e1e5e3d2c1f942d12616fb8062321a5a0dfb817f
BoomshakalakaNL <boomshakalaka.nl@gmail.com>
2020-10-17 16:01:41 +1000

    Database.php now reads data from config.ini

--- src/Database.php
+++ src/Database.php
@@ -2,26 +2,35 @@
 
 namespace ScriptTask;
 
+use Exception;
 use PDO;
 use PDOException;
 
 class Database
 {
-    private $engine;
-    private $host;
-    private $database;
-    private $user;
-    private $pass;
+    private string $engine;
+    private string $host;
+    private string $database;
+    private string $user;
+    private string $pass;
 
     // Constructor is private because only getLink() function should be user outside this class
     private function __construct()
     {
-        // NOTE: Load from config.ini
-        $this->engine = 'mysql';
-        $this->host = 'localhost';
-        $this->database = 'todo';
-        $this->user = 'root';
-        $this->pass = '';
+        try {
+            if(! $config = parse_ini_file(__DIR__."\..\config.ini", true) )
+            {
+                exit("Could not read \"config.ini\". Program can't continue.");
+            }
+            $this->engine = $config['dsn']['engine'];
+            $this->host = $config['dsn']['host'];
+            $this->database = $config['dsn']['database'];
+            $this->user = $config['db_user'];
+            $this->pass = $config['db_password'];
+        }
+        catch (Exception $e) {
+            throw new Exception($e);
+        }
     }
 
     public static function getLink($user = null, $pass = null, $host = null): PDO
